2734019f9d5351ceba1e54cc533cec2689b73ecc
Bernard Mordan <bernardmordan@gmail.com>
2020-05-19 16:06:18 +0100

    new app name without insta

--- index.js
+++ index.js
@@ -19,21 +19,22 @@ app.get("/app", (req, res) => {
 })
 
 app.get("/auth", (req, res) => {
-    const {code} = req.query
+    console.log(JSON.stringify(req.params, null, 3))
+    const {code} = req.params
     fetch(`https://api.instagram.com/oauth/access_token`, {
         method: 'POST',
         body: JSON.stringify({
             client_id: 2637301779709157,
             client_secret: process.env.INSTAGRAM_SECRET,
             grant_type: 'authorization_code',
-            redirect_uri: 'http://instainter.herokuapp.com/app',
+            redirect_uri: 'https://bernards-photos.herokuapp.com/app',
             code: code
         })
     })
-    .then(res => res.json())
-    .then(instagram => {
-        res.send(`OK we have an access token: ${instagram.access_token}`)
-    })
+    // .then(res => res.json())
+    // .then(instagram => {
+    //     res.send(`OK we have an access token: ${instagram.access_token}`)
+    // })
 })
 
-app.listen(PORT, () => console.log(`Instagram intergration ready on port ${PORT} 🎉 I got Secrets too ${process.env.INSTAGRAM_SECRET}`))
+app.listen(PORT, () => console.log(`Instagram intergration ready on port ${PORT} 🎉`))
