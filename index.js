8f66d2dc47571d923356b6db0dabb37cd2ac2c30
Bernard Mordan <bernardmordan@gmail.com>
2020-05-19 15:28:05 +0100

    token dance

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express()
 const { PORT = 3000 } = process.env
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-    res.send("Hello You")
+    res.redirect(`https://api.instagram.com/oauth/authorize
+    ?client_id=2637301779709157
+    &redirect_uri=http://instainter.herokuapp.com/auth
+    &scope=user_profile,user_media
+    &response_type=code`)
+})
+
+app.get("/auth", (req, res) => {
+    const {code} = req.query
+    fetch(`https://api.instagram.com/oauth/access_token`, {
+        method: 'POST',
+        body: JSON.stringify({
+            client_id: 2637301779709157,
+            client_secret: process.env.INSTAGRAM_SECRET,
+            grant_type: 'authorization_code',
+            redirect_uri: 'http://instainter.herokuapp.com/app',
+            code: code
+        })
+    })
+    .then(res => res.json())
+    .then(instagram => {
+        res.send(`OK we have an access token: ${instagram.access_token}`)
+    })
 })
 
 app.listen(PORT, () => console.log(`Instagram intergration ready on port ${PORT} 🎉`))
